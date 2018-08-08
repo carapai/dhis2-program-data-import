7c1007a51e27a7c242a7f80a47ea3e1c172ca477
carapai <olupotcharles@gmail.com>
2018-08-08 19:04:47 +0300

    Added functionality for dynamic baseurl

--- src/index.js
+++ src/index.js
@@ -5,13 +5,22 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const baseUrl = 'http://localhost:8080/dhis';
-// const baseUrl = 'https://eidsr.dev.hispuganda.org';
-
+let baseUrl = '';
+if (process.env.NODE_ENV === 'development') {
+    baseUrl = 'http://localhost:8080/dhis/api/29';
+} else {
+    let urlArray = window.location.pathname.split('/');
+    let apiIndex = urlArray.indexOf('api');
+    if (apiIndex > 1) {
+        baseUrl = '/' + urlArray[apiIndex - 1] + '/api/29';
+    } else {
+        baseUrl = '/api/29';
+    }
+}
 
 
 init({
-    baseUrl: baseUrl + '/api/29'
+    baseUrl
 }).then(d2 => {
     ReactDOM.render(
         <App d2={d2} baseUrl={baseUrl}/>, document.getElementById('root'));
